b2f994e5105cd8d85307087529b5369580f2d898
Scott Blake <945258+MScottBlake@users.noreply.github.com>
2025-11-11 20:33:52 -0500

    Fix s3 integration test

--- tests/integration/test_s3.py
+++ tests/integration/test_s3.py
@@ -62,16 +62,16 @@ def test_data() -> RecipeCache:
 async def s3_client(settings: Settings) -> AsyncGenerator[S3Client, None]:
     """Fixture that provides a valid S3Client."""
     session = boto3.Session()
-    s3_client: S3Client
-    with session.client("s3") as s3_client:
-        await s3_client.create_bucket(Bucket=settings.cloud_container_name)
+    s3_client: S3Client = session.client("s3")
 
-        yield s3_client
+    await s3_client.create_bucket(Bucket=settings.cloud_container_name)
 
-        await s3_client.delete_object(
-            Bucket=settings.cloud_container_name, Key=settings.cache_file
-        )
-        await s3_client.delete_bucket(Bucket=settings.cloud_container_name)
+    yield s3_client
+
+    await s3_client.delete_object(
+        Bucket=settings.cloud_container_name, Key=settings.cache_file
+    )
+    await s3_client.delete_bucket(Bucket=settings.cloud_container_name)
 
 
 # Tests
